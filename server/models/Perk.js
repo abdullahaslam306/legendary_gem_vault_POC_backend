06dd9bbbd30a502cdc3978c98f48085693b28996
IqrarH <iqrarhussain471@gmail.com>
2022-03-15 19:32:54 +0500

    Perk Model

--- server/models/Perk.js
+++ server/models/Perk.js
@@ -2,14 +2,11 @@ let mongoose = require("mongoose");
 const mongoosePaginate = require('mongoose-paginate-v2');
 
 let PerkSchema = new mongoose.Schema({
-    description: {type: String},
-    image: {type: String},
-    price: {type: Number},
-    type: {type: String, default: ''},
-    sold: {
-        user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-        date: {type: Date}
-    }
+    description: {type: String},  //Can be Merchandise, Event etc
+    image: {type: String},  //string of image url
+    price: {type: Number},  //Price in No. of gems
+    sold: {type: Number}, //No of items sold for this particular Perk
+    total: {type: Number}, //Total number of items available for this particular Perk
 }, {timestamps: true});
 
 PerkSchema.plugin(mongoosePaginate);
