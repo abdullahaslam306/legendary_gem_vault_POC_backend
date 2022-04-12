4db355b7b72648522cbb06a4649d09b1a01d25eb
IqrarH <iqrarhussain471@gmail.com>
2022-04-12 20:41:50 +0500

    Changes

--- server/routes/api/order.js
+++ server/routes/api/order.js
@@ -25,90 +25,97 @@ router.post('/', auth.required, auth.user, async (req, res, next) => {
     let deductionFromNFTs = 0;
     let quantityArray = [];
 
-    let temp = await Claim.findOne({walletAddress: req.user.walletAddress});
-    if(temp){
-        rewardGems = temp.gems - temp.usedGems;
-    }
+    try{
+        let temp = await Claim.findOne({walletAddress: req.user.walletAddress});
+        if(temp){
+            rewardGems = temp.gems - temp.usedGems;
+        }
 
-    for(let i = 0;i < deductions?.length;i++){
-        deductionFromNFTs += Number(deductions[i].amount)
-    }
-    perks.forEach((perk, index, array) => {
-        Perk.findOne({slug: perk.slug}, (err, result) => {
-            if (!result) {
-                console.log("perk not found for ", perk.slug);
-            }
-
-            totalPrice += (perk.quantity * perk.price);
-
-            quantityArray.push({
-                perk: perk.slug,
-                quantity: Number(perk.quantity)
-            })
-            if((Number(result.quantity) - Number(perk.quantity)) < 0){
-                next(new NotFoundResponse('Requested Perk is out of stock'))
-                return;
-            }
-
-            itemsProcessed++;
-            if(itemsProcessed == array.length){
-
-                claimDeduction = totalPrice - deductionFromNFTs;
-
-                let order = new Order();
-                order.name = req.body.name;
-                order.discord = req.body.discord;
-                order.walletAddress = req.body.walletAddress;
-                order.email = req.body.email;
-                order.remarks = req.body.remarks;
-                order.user = req.user._id;
-                order.date = Date.now();
-                order.quantityArray = quantityArray;
-                // order.perks = perksIds;
-
-
-                for(let i = 0;i < perks.length;i++) {
-                    for(let j = 0;j < Number(perks[i].quantity);j++) {
-                        Coupon.findOneAndUpdate({perk: perks[i].slug, used: false},{used: true},{returnNewDocument:true}, (err, result) => {
-                            if(perks[i].type == 1){
-                                sendCouponEmail(req.body.email, req.body.name, result.coupon);
-                            }
-                        });
-                        // sendCouponEmail(req.body.email, req.body.name, 'testing-coupon');
-                    } 
+        for(let i = 0;i < deductions?.length;i++){
+            deductionFromNFTs += Number(deductions[i].amount)
+        }
+        perks.forEach((perk, index, array) => {
+            Perk.findOne({slug: perk.slug}, (err, result) => {
+                if (!result) {
+                    console.log("perk not found for ", perk.slug);
                 }
 
-                for(let i = 0;i < perks.length;i++) {
-                    Perk.findOne({slug: perks[i].slug}, async(err, result) => {
-                        result.quantity -= Number(perks[i].quantity);
-                        await result.save();
-                    })
+                totalPrice += (perk.quantity * perk.price);
+
+                quantityArray.push({
+                    perk: perk.slug,
+                    quantity: Number(perk.quantity)
+                })
+                if((Number(result.quantity) - Number(perk.quantity)) < 0){
+                    next(new NotFoundResponse('Requested Perk is out of stock'))
+                    return;
                 }
 
-                order.save().then( async() => {
-                    if(claimDeduction != 0){
-                        let userClaimRecord = await Claim.findOne({walletAddress: req.user.walletAddress});
-                        userClaimRecord.usedGems += Number(claimDeduction);
-                        await userClaimRecord.save();
+                itemsProcessed++;
+                if(itemsProcessed == array.length){
+
+                    claimDeduction = totalPrice - deductionFromNFTs;
+
+                    let order = new Order();
+                    order.name = req.body.name;
+                    order.discord = req.body.discord;
+                    order.walletAddress = req.body.walletAddress;
+                    order.email = req.body.email;
+                    order.remarks = req.body.remarks;
+                    order.user = req.user._id;
+                    order.date = Date.now();
+                    order.quantityArray = quantityArray;
+                    // order.perks = perksIds;
+
+
+                    for(let i = 0;i < perks.length;i++) {
+                        for(let j = 0;j < Number(perks[i].quantity);j++) {
+                            Coupon.findOneAndUpdate({perk: perks[i].slug, used: false},{used: true},{returnNewDocument:true}, (err, result) => {
+                                if(perks[i].type == 1){
+                                    sendCouponEmail(req.body.email, req.body.name, result.coupon);
+                                }
+                            });
+                            // sendCouponEmail(req.body.email, req.body.name, 'testing-coupon');
+                        } 
                     }
-                    for(let i = 0;i < deductions?.length;i++){
-                        NFT.findOne({tokenId: deductions[i].asset}, async(err, nft) => {
-                            nft.noOfGems -= Number(deductions[i].amount);
-                            await nft.save();
+
+                    for(let i = 0;i < perks.length;i++) {
+                        Perk.findOne({slug: perks[i].slug}, async(err, result) => {
+                            result.quantity -= Number(perks[i].quantity);
+                            await result.save();
                         })
-
-                        let orderAsset = new OrderAsset();
-                        orderAsset.order = order._id;
-                        orderAsset.address = deductions[i].asset;
-                        orderAsset.gems = deductions[i].amount;
-                        await orderAsset.save();
                     }
-                    next(new OkResponse({order: order}));
-                    
-                }).catch((e) => { next(new BadRequestResponse(e.error)); });
-            }
+
+                    order.save().then( async() => {
+                        if(claimDeduction != 0){
+                            let userClaimRecord = await Claim.findOne({walletAddress: req.user.walletAddress});
+                            userClaimRecord.usedGems += Number(claimDeduction);
+                            await userClaimRecord.save();
+                        }
+                        for(let i = 0;i < deductions?.length;i++){
+                            NFT.findOne({tokenId: deductions[i].asset}, async(err, nft) => {
+                                nft.noOfGems -= Number(deductions[i].amount);
+                                await nft.save();
+                            })
+
+                            let orderAsset = new OrderAsset();
+                            orderAsset.order = order._id;
+                            orderAsset.address = deductions[i].asset;
+                            orderAsset.gems = deductions[i].amount;
+                            await orderAsset.save();
+                        }
+                        next(new OkResponse({order: order}));
+                        
+                    }).catch((e) => { next(new BadRequestResponse(e.error)); });
+                }
+            })
         })
-    })
+    }catch(err){
+        console.log(err);
+        next(new BadRequestResponse(e.error));
+    }
+
+    
 });
 
 
