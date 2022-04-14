208fac144850f84d25315610a36aa9aae4748571
IqrarH <iqrarhussain471@gmail.com>
2022-04-14 15:07:21 +0500

    Changes

--- server/cron/events.js
+++ server/cron/events.js
@@ -1,24 +1,35 @@
 const Moralis = require('moralis/node');
 let { MORALIS_EVENTS } = require('../constants/constants');
-
+let moment = require('moment');
 
 const listStakedEvents = async () => {
     console.log('Getting Staked Events...');
+
+    let date = moment().subtract(15, "minutes").format('YYYY-MM-DDTHH:mm:ss');
     await Moralis.start({ serverUrl: MORALIS_EVENTS.serverUrl, appId: MORALIS_EVENTS.appId });
     const StakedEvents = Moralis.Object.extend("StakedEvents");
     const query = new Moralis.Query(StakedEvents);
-    const results = await query.find();
-    console.log('STAKED', results[0].attributes, results[0].createdAt);
-    listUnstakedEvents();
+    let results = await query.find();
+    results = results.filter(x => moment(x.attributes.block_timestamp).format('YYYY-MM-DDTHH:mm:ss') >= date);
+    console.log('STAKED', results.length);
 }
 
 const listUnstakedEvents = async () => {
     console.log('Getting Unstaked Events...');
+
+    let date = moment().subtract(15, "minutes").format('YYYY-MM-DDTHH:mm:ss');
     await Moralis.start({ serverUrl: MORALIS_EVENTS.serverUrl, appId: MORALIS_EVENTS.appId });
     const UnstakedEvents = Moralis.Object.extend("UnstakedEvents");
     const query = new Moralis.Query(UnstakedEvents);
-    const results = await query.find();
-    console.log('UNSTAKED', results[0].attributes, results[0].createdAt);
+    let results = await query.find();
+    results = results.filter(x => moment(x.attributes.block_timestamp).format('YYYY-MM-DDTHH:mm:ss') >= date);
+    console.log('UNSTAKED', results.length);
 }
 
-listStakedEvents();
+const run = async () => {
+    await listStakedEvents();
+    await listUnstakedEvents();
+}
+
+run();
+
