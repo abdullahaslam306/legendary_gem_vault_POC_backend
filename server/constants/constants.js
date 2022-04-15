14204eb6988ef46fd9a97eaef0d282eabb45790f
IqrarH <iqrarhussain471@gmail.com>
2022-04-15 15:52:08 +0500

    moralis mainnet server

--- server/constants/constants.js
+++ server/constants/constants.js
@@ -9,16 +9,16 @@ const EVENT_TYPE = {
 };
 
 const MORALIS = {
-    serverUrl: "https://rpc11whc2ogq.usemoralis.com:2053/server",
-    appId: "iNsfWaO6RE0vRpBkcPQN2JmOdSm94lMKnaAu2bMV"
+    serverUrl: "https://rpc11whc2ogq.usemoralis.com:2053/server",    //https://z101lcihky72.usemoralis.com:2053/server
+    appId: "iNsfWaO6RE0vRpBkcPQN2JmOdSm94lMKnaAu2bMV"     //FPHda0SBrkhWMudW5wLNWLPzlrRalnjrEKiNpv1q
 };
 
 const MORALIS_EVENTS = {
-    serverUrl: "https://z101lcihky72.usemoralis.com:2053/server",
-    appId: "FPHda0SBrkhWMudW5wLNWLPzlrRalnjrEKiNpv1q"
+    serverUrl: "https://nt3fgkujzou0.usemoralis.com:2053/server",
+    appId: "KeF3XYtdfKnSxZ61utyOdPPh56LJY680KT6Cyqkg"
 }
 
-const NFT_CONTRACT_ADDRESS = "0x8c714199d2ea08cc1f1f39a60f5cd02ad260a1e3"; //0x0D731c7D2247d53a22cE8848F62908991883CF0B  0x8c714199d2ea08cc1f1f39a60f5cd02ad260a1e3  (Mainnet NFT Collection Smart Contract Address)
+const NFT_CONTRACT_ADDRESS = "0x8c714199d2ea08cc1f1f39a60f5cd02ad260a1e3"; //0x0D731c7D2247d53a22cE8848F62908991883CF0B
 
 const CHAIN = "eth";
 
